3d1a2bdf90c50d5e1c4b9c423d75ab5c0346fa56
JohisArias <johismn9-3@hotmail.com>
2023-08-10 17:47:24 -0500

    importacion de objeto

--- Iluminacion, sombras y radiosidad/1.model_loading.fs
+++ Iluminacion, sombras y radiosidad/1.model_loading.fs
@@ -1,11 +1,13 @@
-#version 330 core
-out vec4 FragColor;
+#version 330 core                      // versión 3.30 (OpenGL 3.3 Core Profile)
 
-in vec2 TexCoords;
+out vec4 FragColor;                    // para el color final del fragmento ( color de un píxel en la pantalla)
 
-uniform sampler2D texture_diffuse1;
+in vec2 TexCoords;                     // coordenadas de textura interpoladas desde el vertex shader
+
+uniform sampler2D texture_diffuse1;    // textura de difusión (color) del objeto
 
 void main()
-{    
+{   
+	// toma el color de la textura en las coordenadas interpoladas, y lo asigna al fragment
     FragColor = texture(texture_diffuse1, TexCoords);
 }
