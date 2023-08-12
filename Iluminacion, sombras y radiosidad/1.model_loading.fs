087a5310711aa1937cad85208f9d30bd71f9d2c9
JohisArias <johismn9-3@hotmail.com>
2023-08-12 17:27:37 -0500

    segundo punto de luz

--- Iluminacion, sombras y radiosidad/1.model_loading.fs
+++ Iluminacion, sombras y radiosidad/1.model_loading.fs
@@ -3,7 +3,6 @@
 out vec4 FragColor;                    // para el color final del fragmento ( color de un píxel en la pantalla)
 
 in vec2 TexCoords;                     // coordenadas de textura interpoladas desde el vertex shader
-in vec3 FragPos;
 in vec3 Normal;
 in vec3 color;
 in vec3 crntPos;
@@ -15,33 +14,50 @@ uniform sampler2D texture_diffuse1;    // textura de difusión (color) del objeto
 
 uniform vec3 viewPosition;             // posicion de camara
 
-uniform vec4 lightColor;               // color of the light from the main function
-// Luz 1
-uniform vec3 Light1Pos;
+struct Light {
+    vec3 position;
+    vec4 color;
+};
+
+uniform Light lights[2];               // Array de luces (Light1 y Light2)
 
 void main()
 {
     // ambient lighting
     float ambient = 0.20f;
     
-    // diffuse lighting
+    // for diffuse lighting
     vec3 normal = normalize(Normal);
-    vec3 lightDirection = normalize(Light1Pos - crntPos);
-    float diffuse = max(dot(normal, lightDirection), 0.0f);
     
-    // specular lighting
+    // for specular lighting
     float specularLight = 0.50f;
     vec3 viewDirection = normalize(viewPosition - crntPos);
-    vec3 reflectionDirection = reflect(-lightDirection, normal);
-    float specAmount = pow(max(dot(viewDirection, reflectionDirection), 0.0f), 8);
-    float specular = specAmount * specularLight;
+    float shininess = 8.0;          // coeficiente de brillo
 
-    float phongLighting = ambient + diffuse + specular;
+    vec3 phongLighting = vec3(0.0);
 
-    if(useTexture){     // textura en objeto
-        FragColor = texture(texture_diffuse1, TexCoords) * lightColor * phongLighting;
-   }
-   else {               // sin textura para piso
-        FragColor = vec4(surfaceColor, 1.0) * lightColor * phongLighting;        
-   }
+    for (int i = 0; i < 2; ++i) {
+        // diffuse lighting
+        vec3 lightDirection = normalize(lights[i].position - crntPos);
+        float diff = max(dot(normal, lightDirection), 0.0); // el maximo entre (producto punto de N con L) y (0)
+
+        // specular lighting
+        vec3 reflectionDirection = reflect(-lightDirection, normal);
+        float specAmount = pow(max(dot(viewDirection, reflectionDirection), 0.0), shininess); // potencia de [el maximo entre (producto punto de V con R) y (0)] elevado a brillo
+        float spec = specAmount * specularLight;
+
+        // Cálculo de luz ambiental, difusa y especular
+        vec3 ambient = ambient * lights[i].color.rgb;
+        vec3 diffuse = diff * lights[i].color.rgb;
+        vec3 specular = spec * lights[i].color.rgb;
+
+        // Sumar las componentes de iluminación (modelo Phong) al resultado final
+        phongLighting += ambient + diffuse + specular;
+    }
+    
+    if (useTexture) {     // textura en objeto
+        FragColor = texture(texture_diffuse1, TexCoords) * vec4(phongLighting, 1.0);
+    } else {               // sin textura para piso
+        FragColor = vec4(surfaceColor, 1.0) * vec4(phongLighting, 1.0);
+    }
 }
--- Iluminacion, sombras y radiosidad/2.light.vs
+++ Iluminacion, sombras y radiosidad/2.light.vs
@@ -5,7 +5,13 @@ layout (location = 0) in vec3 aPos;
 uniform mat4 model;
 uniform mat4 camMatrix;
 
+uniform vec3 LightPosition1;            // Valor de la posición de la primera luz pasado desde el main
+uniform vec3 LightPosition2;            // Valor de la posición de la segunda luz pasado desde el main
+
+out vec3 FragPos;                       // Variable de salida para la posición del fragmento
+
 void main()
 {
-	gl_Position = camMatrix * model * vec4(aPos, 1.0f);
+    gl_Position = camMatrix * model * vec4(aPos, 1.0f);
+    FragPos = vec3(model * vec4(aPos, 1.0));
 }
